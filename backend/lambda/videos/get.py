65bd9918d0424700576cd39f4017e5176d329111
rkawasaki02 <ryuji.cloud02@gmail.com>
2026-05-28 22:12:48 +0900

    feat: DynamoDBからデータを取得する際のDecimal型に対応

--- backend/lambda/videos/get.py
+++ backend/lambda/videos/get.py
@@ -1,26 +1,29 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('videogarage-videos')
 
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super().default(obj)
 
 def lambda_handler(event, context):
-    # CognitoのJWTトークンからuserIdを取得
     user_id = event['requestContext']['authorizer']['jwt']['claims']['sub']
 
-    # DynamoDBからそのユーザーの動画一覧を取得
     response = table.query(
         KeyConditionExpression=Key('userId').eq(user_id)
     )
-    
-    # 取得した動画リストを返す
+
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': 'https://videogarage.jp',
-            'Content-Type': 'applivation/json'
+            'Content-Type': 'application/json'
         },
-        'body': json.dumps(response['Items'])
+        'body': json.dumps(response['Items'], cls=DecimalEncoder)
     }
